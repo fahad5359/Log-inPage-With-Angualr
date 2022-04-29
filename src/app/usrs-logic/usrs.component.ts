9e50cb53be30a71a653944ad060bb5e0f073c255
fahad5359 <fabbgm5359@gmail.com>
2022-04-29 20:41:32 +0300

    fnshed logic/ only style left

--- src/app/usrs-logic/usrs.component.ts
+++ src/app/usrs-logic/usrs.component.ts
@@ -2,43 +2,59 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'usrs-logic',
-    templateUrl:'./usrs.component.html',
+    templateUrl: './usrs.component.html',
     styleUrls: ['./usrs.component.css']
 })
-export class UsrsLogic{
+export class UsrsLogic {
     // LogIN
-    userNem=""
-    passWrd=""
-    eml=""
-    nmbr=""
-  
+    userNem = ""
+    passWrd = ""
+    eml = ""
+    nmbr = ""
 
-// String Interpolation
-EmailOrPhone="ByEmail"
 
-// PropertyBinding
-    btnStts=true
-    emailType="Email"
-    phoneType="number"
- 
-    phone="number"
-    placeHolder="Enter Email"
+    //Array
+    usrsArray=["fahad"]; 
 
-// Boolean/not Binded
-    sumbitt=false
+    // String Interpolation
+    EmailOrPhone = "ByEmail"
+
+    // PropertyBinding
+    btnStts = true
+    emailType = "Email"
+    phoneType = "number"
+
+    phone = "number"
+    placeHolder = "Enter Email"
+
+    // Boolean/not Binded
+    sumbitt = false
+    bool_eml = false
+    bool_phne = false
 
 
     // methods
-    getUsrNem(){
-        this.sumbitt=true
+    getUsrNem() {
+        this.sumbitt = true
+        this.usrsArray.push(this.userNem)
 
     }
-    toPhone_numb(){
-       
+    // to get the the phone nuber only
+    toPhone_numb() {
+        if (this.bool_eml = true) {
+            this.bool_phne = false
+
+        }
 
     }
-    revertEmlPhon(){
-     
+    // to get the the Email only
+    revertEmlPhon() {
+        if (this.bool_phne = true) {
+            this.bool_eml = false
+
+        }
+
+
 
     }
 
